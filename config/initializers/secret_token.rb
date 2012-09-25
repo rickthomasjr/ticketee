3bd3bb977d098d64c5ba138ec6b9a10d9a241a90
Rick Thomas <rthomas@igodigital.com>
2012-09-25 09:17:21 -0400

    upgrade to rails 3.2.8

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ticketee::Application.config.secret_token = '9dde66265973b99d61290898be06072d70a6da9e7b605d374a3460d0b9246a4f91e102476696d61b1bc179073077c99bc5dc6d295d32d9ff2374c753d4102efc'
+Ticketee::Application.config.secret_token = 'b0bc717c0c59e4b3556f965ad08964dc24a084a63a29104ebd8d327493546850485cb21ec52c0b75d898acb67c0331abde0b2d366b38b0c2c0734055afebcd56'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Ticketee::Application.config.session_store :cookie_store, :key => '_ticketee_session'
+Ticketee::Application.config.session_store :cookie_store, key: '_ticketee_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
