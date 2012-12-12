e287aae220d9309f1a12142de6cc80eaa6a6413e
Rick Thomas <rthomas@igodigital.com>
2012-12-12 13:45:42 -0500

    added project level permissions

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,10 @@
 class Project < ActiveRecord::Base
   has_many :tickets
+  has_many :permissions, :as => :thing
   attr_accessible :name
   validates :name, :presence => true
+
+  scope :readable_by, lambda {|user| joins(:permissions).where(:permissions => {:action=>"view", :user_id => user.id})}
+
+
 end
